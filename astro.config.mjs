fbb15d0958c99645b5954f6c19650c08a6680126
Emilio Tapia <emta.develop@gmail.com>
2023-03-21 08:39:21 -0500

    fix some errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 import image from "@astrojs/image";
 
+import astroI18next from "astro-i18next";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), image()]
+  integrations: [svelte(), image(), astroI18next()]
 });
